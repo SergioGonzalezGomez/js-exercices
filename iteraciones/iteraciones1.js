104d162d12b593a2500bb980579fb097735c7577
Sergio González Gómez <sergiogonzalezgomez15@gmail.com>
2021-12-09 18:57:15 +0100

    ITERACIONES2

--- iteraciones/iteraciones1.js
+++ iteraciones/iteraciones1.js
@@ -81,4 +81,59 @@ car2.finalPrice = car2.basePrice + globalBasePrice;
 
 let rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
 rickAndMortyCharacters.splice(1, 1);
-console.log(rickAndMortyCharacters);
+//console.log(rickAndMortyCharacters);
+
+
+
+// ITERACIONES 5 CONDICIONALES
+
+//5.1
+const number1 = 10;
+const number2 = 20;
+const number3 = 2;
+
+if (number2/number1 == 2) {
+    //console.log("number2 dividido entre number1 es igual a 2");
+  }
+
+  if (number1 !== number2) {
+    //console.log("number1 es estrictamente distinto a number2");
+  }
+
+  if (number3 != number1) {
+    //console.log("number3 es distinto number1");
+  } 
+  
+  if (number3 * 5 == number1) {
+   // console.log("number3 por 5 es igual a number1");
+  }
+  if (number3 * 5 == number1 && number1 * 2 == number2) {
+    //console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
+  }
+  if (number2 / 2 == number1 || number1 / 5 == number3) {
+    //console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
+  } 
+  
+
+ // ITERACIONES 6 BUCLES 
+//6.1
+for (let i = 0; i <= 9; i++){
+    //console.log(i);
+}
+
+
+//6.2
+for (let i = 0; i <= 9; i++){
+    if (i % 2 == 0){
+        //console.log(i);
+    }
+}
+
+//6.3
+for (let i = 0; i <= 10; i++){
+    if (i != 10){
+        console.log("Intentando dormir");
+    } else {
+        console.log("Dormido!");
+    }
+}
